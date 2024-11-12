18ad369c15ce03fff895f3815964e0ee62d4fd6d
Hung TM <hung.tran6018@gmail.com>
2024-11-12 23:14:59 +0700

    footer

--- mvc-oop-basic/mvc-oop-basic/controllers/HomeController.php
+++ mvc-oop-basic/mvc-oop-basic/controllers/HomeController.php
@@ -2,6 +2,17 @@
 
 class HomeController
 {
-    
+    public $modelSanpham;
+
+    public function __construct()
+    {
+        $this->modelSanpham = new SanPham();   
+    }
+    public function home(){
+        require_once './views/home.php';
+    }
+    public function trangChu(){
+        echo "Day laf trang chu cua tooi ";
+    }
 
 }
