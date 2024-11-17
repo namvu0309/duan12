38fda55ab5432b6e0856b2a7d1cd605f698d56ae
Hung TM <hung.tran6018@gmail.com>
2024-11-17 09:43:30 +0700

    ss

--- mvc-oop-basic/commons/env.php
+++ mvc-oop-basic/commons/env.php
@@ -3,7 +3,7 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL', 'http://localhost/mvc-oop-basic/mvc-oop-basic/');
+define('BASE_URL', 'http://localhost/duan/mvc-oop-basic/mvc-oop-basic/');
 // duong dan vao phan Admin
 define('BASE_URL_ADMIN', 'http://localhost/duan/mvc-oop-basic/mvc-oop-basic/admin/');
 
