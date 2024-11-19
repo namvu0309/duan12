f4fbc7d9c0a239fcc51ea9558eaad951c59387eb
Your Name <you@example.com>
2024-11-19 22:57:56 +0700

    ban

--- mvc-oop-basic/commons/env.php
+++ mvc-oop-basic/commons/env.php
@@ -3,14 +3,14 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL', 'http://localhost/duan/mvc-oop-basic/mvc-oop-basic/');
+define('BASE_URL', 'http://localhost/duan/mvc-oop-basic/');
 // duong dan vao phan Admin
-define('BASE_URL_ADMIN', 'http://localhost/duan/mvc-oop-basic/mvc-oop-basic/admin/');
+define('BASE_URL_ADMIN', 'http://localhost/duan/mvc-oop-basic/admin/');
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'duan');  // Tên database
+define('DB_NAME', 'duan1');  // Tên database
 
 define('PATH_ROOT', __DIR__ . '/../');
